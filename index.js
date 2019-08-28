53762bed79a3b9458f4473ef850fe500f67bb4c9
Geoffrey Pascal <geoffrey.pascal@fr.ibm.com>
2019-08-28 14:39:03 +0200

    Helloworld website added

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const os = require('os');
+
+String.prototype.toHHMMSS = function () {
+    var sec_num = parseInt(this, 10); // don't forget the second param
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    return hours+':'+minutes+':'+seconds;
+}
+
+app.use(express.static('static/public'));
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.get('/', function (req, res) {
+  res.render(__dirname + "/static/template/index.html", {arch: os.arch(), hostname: os.hostname(), uptime: String(os.uptime()).toHHMMSS() });
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
