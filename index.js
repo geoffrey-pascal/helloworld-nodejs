7012fb5303382668a0fcb5a769c46ba977dfee79
Geoffrey Pascal <geoffrey.pascal@fr.ibm.com>
2019-08-28 18:09:14 +0200

    Sushiweb added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.set('view engine', 'html');
 
 app.get('/', function (req, res) {
   console.log("Serving new request")
-  res.render(__dirname + "/static/template/index.html", {arch: os.arch(), hostname: os.hostname() });
+  res.render(__dirname + "/static/template/index_ppc64le.html", {arch: os.arch(), hostname: os.hostname() });
 })
 
 app.listen(3000, function () {
